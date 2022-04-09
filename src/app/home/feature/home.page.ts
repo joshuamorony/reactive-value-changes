18e3847a874e2fcc523754e1278b11e89e5a174e
Josh Morony <joshua.morony@gmail.com>
2022-04-09 11:41:06 +0930

    initial commit

--- src/app/home/feature/home.page.ts
+++ src/app/home/feature/home.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap, takeUntil } from 'rxjs/operators';
 import { HomeStore } from '../data-access/home.store';
 import { ExampleService } from '../data-access/example-service.service';
-import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -24,34 +23,14 @@ export class HomePage implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.homeStore.fieldOneValueChangesEffect(
-      this.myForm.get('fieldOne').valueChanges.pipe(
-        // Get a related value from some API
+    this.myForm
+      .get('fieldOne')
+      .valueChanges.pipe(
         switchMap((val) => this.exampleService.getRelatedValue(val)),
-        // Set that value
-        tap({
-          next: (value) => this.myForm.get('fieldTwo').setValue(value),
-        })
+        takeUntil(this.homeStore.destroy$)
       )
-    );
-
-    // const obs1$ = timer(1000, 1000).pipe(
-    //   tap({
-    //     next: (value) => this.myForm.get('fieldTwo').setValue(value),
-    //   })
-    // );
-    // const obs2$ = timer(1000, 1300).pipe(
-    //   tap({
-    //     next: (value) => this.myForm.get('fieldTwo').setValue(value),
-    //   })
-    // );
-    // const obs3$ = timer(1000, 1800).pipe(
-    //   tap({
-    //     next: (value) => this.myForm.get('fieldTwo').setValue(value),
-    //   })
-    // );
-    // this.homeStore.subscribeTo(obs1$);
-    // this.homeStore.subscribeTo(obs2$);
-    // this.homeStore.subscribeTo(obs3$);
+      .subscribe((value) => {
+        this.myForm.get('fieldTwo').setValue(value);
+      });
   }
 }
