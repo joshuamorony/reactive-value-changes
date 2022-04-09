e5b5f69c3fbaaa5840da6ff8351ac3af887d8a96
Josh Morony <joshua.morony@gmail.com>
2022-04-09 11:33:31 +0930

    initial commit

--- src/app/home/feature/home.page.ts
+++ src/app/home/feature/home.page.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { switchMap, tap } from 'rxjs/operators';
-import { HomeStore } from '../data-access/home.store';
 import { ExampleService } from '../data-access/example-service.service';
-import { timer } from 'rxjs';
+import { Subject } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  providers: [HomeStore],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
+  private destroy$ = new Subject<boolean>();
+
   myForm = this.fb.group({
     fieldOne: [],
     fieldTwo: [],
@@ -19,39 +19,22 @@ export class HomePage implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private homeStore: HomeStore,
     private exampleService: ExampleService
   ) {}
 
   ngOnInit() {
-    this.homeStore.fieldOneValueChangesEffect(
-      this.myForm.get('fieldOne').valueChanges.pipe(
-        // Get a related value from some API
+    this.myForm
+      .get('fieldOne')
+      .valueChanges.pipe(
         switchMap((val) => this.exampleService.getRelatedValue(val)),
-        // Set that value
-        tap({
-          next: (value) => this.myForm.get('fieldTwo').setValue(value),
-        })
+        takeUntil(this.destroy$)
       )
-    );
+      .subscribe((value) => {
+        this.myForm.get('fieldTwo').setValue(value);
+      });
+  }
 
-    // const obs1$ = timer(1000, 1000).pipe(
-    //   tap({
-    //     next: (value) => this.myForm.get('fieldTwo').setValue(value),
-    //   })
-    // );
-    // const obs2$ = timer(1000, 1300).pipe(
-    //   tap({
-    //     next: (value) => this.myForm.get('fieldTwo').setValue(value),
-    //   })
-    // );
-    // const obs3$ = timer(1000, 1800).pipe(
-    //   tap({
-    //     next: (value) => this.myForm.get('fieldTwo').setValue(value),
-    //   })
-    // );
-    // this.homeStore.subscribeTo(obs1$);
-    // this.homeStore.subscribeTo(obs2$);
-    // this.homeStore.subscribeTo(obs3$);
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 }
